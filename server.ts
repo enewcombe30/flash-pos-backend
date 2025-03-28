f10b35281b2b4c922af6206c2c70e68d5248af24
elijah newcombe <78599237+enewcombe30@users.noreply.github.com>
2025-03-28 18:56:03 +0000

    route tweaks

--- server.ts
+++ server.ts
@@ -11,5 +11,9 @@ app.use(cors());
 
 app.use("/api/recipes", recipeRoutes);
 
+app.get("/test", (req, res) => {
+  res.send("Server is working!");
+});
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
